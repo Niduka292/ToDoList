7ca3b0d7ea677b225789c206e52f50ed2ec7534c
Niduka Jayathunga <nidukajayathunga886@gmail.com>
2025-04-15 16:56:04 +0530

    Add feature to switch themes and add final touches

--- to-do-list-app/src/ToDoList.jsx
+++ to-do-list-app/src/ToDoList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import trashIcon from "./assets/deleteIcon.png"
 
 function ToDoList() {
@@ -8,6 +8,21 @@ function ToDoList() {
         { id: 2, taskName: "Eat Breakfast", isChecked: false },
     ]);
     const [userInput, setUserInput] = useState("");
+    const [isDarkTheme, setIsDarkTheme] = useState(false);
+    const [themeOption, setThemeOption] = useState("Dark Mode")
+
+    function handleThemeToggle(){
+        setIsDarkTheme(!isDarkTheme);
+        
+    }
+
+    useEffect(() => {
+        setThemeOption(isDarkTheme ? "Light Mode" : "Dark Mode");
+    },[isDarkTheme]);
+
+    useEffect(() => {
+        document.body.classList.toggle("dark", isDarkTheme);
+    },[isDarkTheme]);
 
     function handleInput(event) {
         setUserInput(event.target.value);
@@ -24,110 +39,116 @@ function ToDoList() {
             const checked = tasks.filter(task => task.isChecked);
             const unchecked = tasks.filter(task => !task.isChecked);
 
-            const updatedTasks = [...unchecked,newTask,...checked];
+            const updatedTasks = [...unchecked, newTask, ...checked];
             setTasks(updatedTasks);
             setUserInput("");
-            
+
         }
     }
 
     function removeTask(id) {
-        const updated = tasks.filter((_, i) => i !== id);
+        const updated = tasks.filter(task => task.id !== id);
         setTasks(updated);
     }
 
     function moveTaskUp(index) {
         const updatedTasks = [...tasks];
-        if(index > 0){
-            [updatedTasks[index],updatedTasks[index - 1]]=
-            [updatedTasks[index - 1],updatedTasks[index]];
+        if (index > 0) {
+            [updatedTasks[index], updatedTasks[index - 1]] =
+                [updatedTasks[index - 1], updatedTasks[index]];
             setTasks(updatedTasks);
         }
-        
+
 
     }
 
     function moveTaskDown(index) {
         const updatedTasks = [...tasks];
-        if(index < tasks.length - 1){
-            [updatedTasks[index + 1],updatedTasks[index]]=
-            [updatedTasks[index],updatedTasks[index + 1]];
+        if (index < tasks.length - 1) {
+            [updatedTasks[index + 1], updatedTasks[index]] =
+                [updatedTasks[index], updatedTasks[index + 1]];
             setTasks(updatedTasks);
 
-        }else{
+        } else {
             setTasks(updatedTasks);
         }
     }
 
     function handleTaskCheck(index) {
-        const updated = tasks.map((task,i) =>
+        const updated = tasks.map((task, i) =>
             i === index ? { ...task, isChecked: !task.isChecked } : task
         );
         const rendered = [
             ...updated.filter(task => !task.isChecked),
             ...updated.filter(task => task.isChecked)
         ]
-        
+
         setTasks(rendered);
     }
 
     return (
-        <div className="to-do-list-container">
-            <h2 className="title">To-Do List</h2>
-            <div className="display-tasks">
-                <ul style={{ listStyleType: "none", padding: 0 }}>
-                    {tasks.map((task, index) =>
-                        <li
-                            key={index}>
-                            <div className="task-row">
-                                <input type="checkbox"
-                                    className="task-checkbox"
-                                    checked={task.isChecked}
-                                    onChange={() => handleTaskCheck(index)}
-                                    
-                                ></input>
+        <>
+            <div className="header-div">
+                <button className="theme-toggle"
+                        onClick={handleThemeToggle}
+                >{themeOption}</button>
+            </div>
+            <div className="to-do-list-container">
+                <h2 className="title">To-Do List</h2>
+                <div className="display-tasks">
+                    <ul style={{ listStyleType: "none", padding: 0 }}>
+                        {tasks.map((task, index) =>
+                            <li
+                                key={index}>
+                                <div className="task-row">
+                                    <input type="checkbox"
+                                        className="task-checkbox"
+                                        checked={task.isChecked}
+                                        onChange={() => handleTaskCheck(index)}
 
-                                <span className="task-text" style={{
-                                    color: task.isChecked ? "grey" : "black",
-                                    textDecoration: task.isChecked ? "line-through" : "none"
-                                }}>
-                                    {task.taskName}
-                                </span>
+                                    ></input>
 
-                            </div>
-                            <button className="remove-button"
+                                    <span className="task-text" style={{
+                                        textDecoration: task.isChecked ? "line-through" : "none",
+                                        color: task.isChecked ? "grey":""
+                                    }}>
+                                        {task.taskName}
+                                    </span>
+
+                                </div>
+                                <button className="remove-button"
                                     onClick={() => removeTask(index)}>
-                                    <img src={trashIcon} 
-                                    alt="Delete" 
-                                    width="20px"
-                                     height="20px"></img>
-                            </button>
+                                    <img src={trashIcon}
+                                        alt="Delete"
+                                        width="20px"
+                                        height="20px"></img>
+                                </button>
 
-                            <button className="task-move-up"
+                                <button className="task-move-up"
                                     onClick={() => moveTaskUp(index)}>⬆️</button>
-                            <button className="task-move-down"
+                                <button className="task-move-down"
                                     onClick={() => moveTaskDown(index)}>⬇️</button>
 
-                        </li>
-                    )}
-                </ul><br />
+                            </li>
+                        )}
+                    </ul><br />
+                </div>
+                <div className="add-new-task">
+                    <input className="new-task"
+                        value={userInput}
+                        placeholder="  e.g. Study"
+                        onChange={handleInput}
+                        type="text"
+                        onKeyDown={(e) => {
+                            if (e.key === 'Enter') {
+                                addTask();
+                            }
+                        }}
+                    ></input>
+                    <button className="add-task-button" onClick={addTask}>Add</button>
+                </div>
             </div>
-            <div className="add-new-task">
-                <input className="new-task"
-                    value={userInput}
-                    placeholder="  e.g. Study"
-                    onChange={handleInput}
-                    type="text"
-                    onKeyDown={(e) => {
-                        if(e.key === 'Enter'){
-                            addTask();
-                        }
-                    }}
-                ></input>
-                <button className="add-task-button" onClick={addTask}>Add</button>
-            </div>
-        </div>
-
+        </>
     )
 
 }
